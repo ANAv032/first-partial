fd8a20248b513cbed02abf47c1d926d7b14a3fd8
ANAv032 <145053328+ANAv032@users.noreply.github.com>
2023-09-19 09:53:35 -0600

    Create coding-rush.py

---
+++ coding-rush.py
@@ -0,0 +1,7 @@
+def name():
+print("give me a word")
+  variable = input()
+  print("give me another word, please")
+  variable = input()
+  result = word[::-1]
+  print("thats your word now", result)
